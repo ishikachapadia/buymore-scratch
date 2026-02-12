2182896b85a4faa70958dd4379ab128f49cdc0c0
Ishika <ishikachapadia@gmail.com>
2026-02-11 20:35:31 -0500

    Created basic login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,31 @@
 import {useNavigate} from 'react-router-dom';
-
-function NameForm(props){
-
+export default function Login() {
+    
     const navigate = useNavigate();
 
-    function goToThanks(e){
+    function thanks(e){
         e.preventDefault();
+        console.log("Thank you user!");
         navigate('/thanks');
     }
-    return (
-        <form onSubmit={goToThanks}>
-            <section>
-                <label>Salutation:
-                    <input type="text" onChange={props.handleSalutationChange}/>
-                </label>
-                <label>Name:
-                    <input type="text" onChange={props.handleNameChange}/>
-                </label>
-            </section>
-            <input type="submit" value="Submit" />
-        </form>
-    )
-}
 
-export default NameForm;
+    return (
+        <section className="login-bg">
+
+            <main className="login-main">
+                <div className="login-box">
+                    <h1 className="login-title">HI, John!</h1>
+                    <p className="login-subtitle">Welcome back ! Ready to have a chance to win?</p>
+                    <form onSubmit={thanks} className="login-form">
+                        <label className="login-label">Email Address</label>
+                            <input type="email" className="login-input" />
+                        <label className="login-label">Password</label>
+                            <input type="password" className="login-input" />
+                        <button type="submit" value="Submit" className="login-btn">Log in</button>
+                    </form>
+                    <div className="login-signup">Don't have an account? <a href="/register">Sign up here.</a></div>
+                </div>
+            </main>
+        </section>
+    );
+}
